eed91f62f4a5c535a5f3a84b5fc4707f50a1fa41
Umut Toker <utoker@gmail.com>
2021-07-06 20:16:00 -0400

    add clean code

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+import { getDues } from "./getdues.js";
+import { getExpenses } from "./getexpenses.js";
 const db = firebase.firestore();
 
 const getResidentButton = document.querySelector(".get-resident-button");
@@ -30,66 +32,20 @@ async function createExpense() {
   getExpenses();
 }
 
-async function getExpenses() {
-  document.querySelector(".items-title").textContent = `${expenseMonth.value}`;
-  const itemsSpan = document.querySelectorAll(".items span");
-  itemsSpan.forEach((item) => {
-    item.remove();
-  });
-  const items = document.querySelector(".items");
-  const createSpan = function (doc) {
-    const a = document.createElement("span");
-    items.append(a);
-    a.textContent = `${doc.id} ${doc.data().key}`;
-  };
-  try {
-    let expenses = await db
-      .collection("expenses")
-      .doc(`2021`)
-      .collection(`${expenseMonth.value}`)
-      .get();
-    expenses.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      createSpan(doc);
-    });
-  } catch (err) {
-    console.log("Error getting document:", err);
-  }
-}
-
-function getResident() {
+async function getResident() {
   const inputValue = document.querySelector(".resident-input").value;
   const residentInfo = document.querySelector(".resident-info");
   const docRef = db.collection("residents").doc(`${inputValue}`);
   const id = document.querySelector(".resident-id");
-  docRef
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        id.textContent = `${doc.id}`;
-        residentInfo.textContent = `isim: ${doc.data().name} 
-        tel: ${doc.data().tel}`;
-      } else {
-        // doc.data() will be undefined in this case
-        residentInfo.textContent = `bulunamadi`;
-        console.log("No such document!");
-      }
-    })
-    .catch((error) => {
-      console.log("Error getting document:", error);
-    });
-
-  db.collection("residents")
-    .doc(`${inputValue}`)
-    .collection("2021")
-    .get()
-    .then((qs) => {
-      qs.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        document.querySelector(`.${doc.id}`).textContent = `${doc.id}: 
-        ${doc.data().due}`;
-      });
-    });
+  try {
+    const r = await docRef.get();
+    id.textContent = `${r.id}`;
+    residentInfo.textContent = `isim: ${r.data().name} 
+            tel: ${r.data().tel}`;
+  } catch (err) {
+    console.log("Error getting document:", err);
+  }
+  getDues();
 }
 
 async function updateDue() {
@@ -105,16 +61,5 @@ async function updateDue() {
   } catch (err) {
     console.log("Error getting document:", err);
   }
-  db.collection("residents")
-    .doc(`${id.textContent}`)
-    .collection("2021")
-    .get()
-    .then((qs) => {
-      qs.forEach((doc) => {
-        console.log(doc.data().due);
-        // doc.data() is never undefined for query doc snapshots
-        document.querySelector(`.${doc.id}`).textContent = `${doc.id}: 
-        ${doc.data().due}`;
-      });
-    });
+  getDues();
 }
