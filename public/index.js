a639bc32c5c265c9cdc0265da15383e9fd428bd3
Umut Toker <utoker@gmail.com>
2021-07-06 21:10:27 -0400

    add modules

--- public/index.js
+++ public/index.js
@@ -1,65 +1,27 @@
-import { getDues } from "./getdues.js";
-import { getExpenses } from "./getexpenses.js";
-const db = firebase.firestore();
+import { getExpenses } from "./modules/gete.js";
+import { createExpense } from "./modules/createex.js";
+import { getResident } from "./modules/getre.js";
+import { updateDue } from "./modules/updatedue.js";
 
-const getResidentButton = document.querySelector(".get-resident-button");
-getResidentButton.addEventListener("click", getResident);
-const dueMonth = document.querySelector(".due-month");
-const updateDueButton = document.querySelector(".due-button");
-updateDueButton.addEventListener("click", updateDue);
-const dueInput = document.querySelector(".due-input");
-const expenseMonth = document.querySelector(".expense-month");
-const expenseDocInput = document.querySelector("#expense-doc-input");
-const expenseKeyInput = document.querySelector("#expense-key-input");
-const createExpenseButton = document.querySelector(".create-expense");
-createExpenseButton.addEventListener("click", createExpense);
-expenseMonth.addEventListener("input", getExpenses);
-
-async function createExpense() {
-  try {
-    await db
-      .collection("expenses")
-      .doc("2021")
-      .collection(`${expenseMonth.value}`)
-      .doc(`${expenseDocInput.value}`)
-      .set({
-        key: `${expenseKeyInput.value}`,
-      });
-    console.log("Document successfully written!");
-  } catch (err) {
-    console.log("Error writing document: ", err);
-  }
-  getExpenses();
+window.addEventListener("load", init);
+function init() {
+  document
+    .querySelector(".get-resident-button")
+    .addEventListener("click", getResident);
+  document.querySelector(".due-button").addEventListener("click", updateDue);
+  document
+    .querySelector(".expense-month")
+    .addEventListener("input", getExpenses);
+  document
+    .querySelector(".create-expense")
+    .addEventListener("click", createExpense);
 }
 
-async function getResident() {
-  const inputValue = document.querySelector(".resident-input").value;
-  const residentInfo = document.querySelector(".resident-info");
-  const docRef = db.collection("residents").doc(`${inputValue}`);
-  const id = document.querySelector(".resident-id");
-  try {
-    const r = await docRef.get();
-    id.textContent = `${r.id}`;
-    residentInfo.textContent = `isim: ${r.data().name} 
-            tel: ${r.data().tel}`;
-  } catch (err) {
-    console.log("Error getting document:", err);
-  }
-  getDues();
-}
+document
+  .querySelector(".resident-update-button")
+  .addEventListener("click", updateResInfo);
 
-async function updateDue() {
-  const id = document.querySelector(`.resident-id`);
-  try {
-    await db
-      .collection("residents")
-      .doc(`${id.textContent}`)
-      .collection("2021")
-      .doc(`${dueMonth.value}`)
-      .update({ due: `${dueInput.value}` });
-    console.log("Document successfully updated!");
-  } catch (err) {
-    console.log("Error getting document:", err);
-  }
-  getDues();
+function updateResInfo() {
+  document.querySelector("#update-name").style.display = `block`;
+  document.querySelector("#update-tel").style.display = `block`;
 }
